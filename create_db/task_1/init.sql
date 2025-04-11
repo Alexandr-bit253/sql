da95e25bbbb2ca424bb69ea0187bed8d2bd3aebb
yoda <you@example.com>
2025-04-11 10:33:08 +0300

    rename folder

--- create_db/task_1/init.sql
+++ create_db/task_1/init.sql
@@ -1,37 +1,31 @@
--- cоздание таблицы Genres
 CREATE TABLE IF NOT EXISTS Genres (
     genre_id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL UNIQUE
 );
 
--- cоздание таблицы Executors
 CREATE TABLE IF NOT EXISTS Executors (
     executor_id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL
 );
 
--- cвязующая таблица для cвязи многие-ко-многим между Executors и Genres
 CREATE TABLE IF NOT EXISTS ExecutorGenres (
     executor_id INT REFERENCES Executors(executor_id),
     genre_id INT REFERENCES Genres(genre_id),
     PRIMARY KEY (executor_id, genre_id)
 );
 
--- cоздание таблицы Albums
 CREATE TABLE IF NOT EXISTS Albums (
     album_id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     year INT NOT NULL
 );
 
--- cвязующая таблица для cвязи многие-ко-многим между Executors и Albums
 CREATE TABLE IF NOT EXISTS AlbumsExecutors (
     album_id INT REFERENCES Albums(album_id),
     executor_id INT REFERENCES Executors(executor_id),
     PRIMARY KEY (album_id, executor_id)
 );
 
--- cоздание таблицы Tracks
 CREATE TABLE IF NOT EXISTS Tracks (
     track_id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
@@ -39,14 +33,12 @@ CREATE TABLE IF NOT EXISTS Tracks (
     album_id INT REFERENCES Albums(album_id)
 );
 
--- cоздание таблицы Compilations
 CREATE TABLE IF NOT EXISTS Compilations (
     compilation_id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     year INT NOT NULL
 );
 
--- cвязующая таблица для cвязи многие-ко-многим между Compilations и Tracks
 CREATE TABLE IF NOT EXISTS CompilationTracks (
     compilation_id INT REFERENCES Compilations(compilation_id),
     track_id INT REFERENCES Tracks(track_id),
