cbaa391450b136eb28659a7479610153dd5f0d8a
thion96 <thion96@mail.ru>
2025-05-11 11:14:30 +0300

    complete task 1 in cpp orm

---
+++ cpp_orm/task_1/orm.hpp
@@ -0,0 +1,77 @@
+#include <Wt/Dbo/Dbo.h>
+#include <Wt/Dbo/backend/Postgres.h>
+#include <Wt/WDateTime.h>
+
+namespace dbo = Wt::Dbo;
+
+class Publisher;
+class Book;
+class Shop;
+class Stock;
+class Sale;
+
+typedef dbo::collection<dbo::ptr<Book>> Books;
+class Publisher
+{
+public:
+    std::string name;
+    Books books;
+
+    template <typename Action>
+    void persist(Action &a)
+    {
+        dbo::field(a, name, "name");
+        dbo::hasMany(a, books, dbo::ManyToOne, "publisher");
+    }
+};
+
+typedef dbo::collection<dbo::ptr<Stock>> Stocks;
+class Book
+{
+public:
+    std::string title;
+    dbo::ptr<Publisher> publisher;
+    Stocks stocks;
+
+    template <typename Action>
+    void persist(Action &a)
+    {
+        dbo::field(a, title, "title");
+        dbo::belongsTo(a, publisher, "publisher");
+        dbo::hasMany(a, stocks, dbo::ManyToOne, "book");
+    }
+};
+
+typedef dbo::collection<dbo::ptr<Stock>> ShopStocks;
+class Shop
+{
+public:
+    std::string name;
+    ShopStocks stocks;
+
+    template <typename Action>
+    void persist(Action &a)
+    {
+        dbo::field(a, name, "name");
+        dbo::hasMany(a, stocks, dbo::ManyToOne, "shop");
+    }
+};
+
+typedef dbo::collection<dbo::ptr<Sale>> Sales;
+class Stock
+{
+public:
+    int count;
+    dbo::ptr<Book> book;
+    dbo::ptr<Shop> shop;
+    Sales sales;
+
+    template <typename Action>
+    void persist(Action &a)
+    {
+        dbo::field(a, count, "count");
+        dbo::belongsTo(a, book, "book");
+        dbo::belongsTo(a, shop, "shop");
+        dbo::hasMany(a, sales, dbo::ManyToOne, "stock");
+    }
+};
